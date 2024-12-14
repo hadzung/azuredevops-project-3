de9cbd5227a7238dba7af10b982610039bdd68e8
Ha Van Dung <havandung144@gmail.com>
2024-12-14 23:03:47 +0700

    Initials push

---
+++ terraform/environments/test/main.tf
@@ -0,0 +1,59 @@
+provider "azurerm" {
+  tenant_id       = "${var.tenant_id}"
+  subscription_id = "${var.subscription_id}"
+  client_id       = "${var.client_id}"
+  client_secret   = "${var.client_secret}"
+  features {}
+}
+terraform {
+  backend "azurerm" {
+    storage_account_name = ""
+    container_name       = ""
+    key                  = ""
+    access_key           = ""
+  }
+}
+
+module "network" {
+  source               = "../../modules/network"
+  address_space        = "${var.address_space}"
+  location             = "${var.location}"
+  virtual_network_name = "${var.virtual_network_name}"
+  application_type     = "${var.application_type}"
+  resource_type        = "NET"
+  resource_group       = "${var.resource_group}"
+  address_prefix_test  = "${var.address_prefix_test}"
+}
+
+module "nsg-test" {
+  source           = "../../modules/networksecuritygroup"
+  location         = "${var.location}"
+  application_type = "${var.application_type}"
+  resource_type    = "NSG"
+  resource_group   = "${var.resource_group}"
+  subnet_id        = "${module.network.subnet_id_test}"
+  address_prefix_test = "${var.address_prefix_test}"
+}
+module "appservice" {
+  source           = "../../modules/appservice"
+  location         = "${var.location}"
+  application_type = "${var.application_type}"
+  resource_type    = "AppService"
+  resource_group   = "${var.resource_group}"
+}
+module "publicip" {
+  source           = "../../modules/publicip"
+  location         = "${var.location}"
+  application_type = "${var.application_type}"
+  resource_type    = "publicip"
+  resource_group   = "${var.resource_group}"
+}
+module "vm" {
+  source              = "../../modules/vm"
+  resource_group = "${var.resource_group}"
+  location            = "${var.location}"
+  resource_type       = "AzureVM"
+  subnet_id           = "${module.network.subnet_id_test}"
+  security_group_id   = "${module.nsg-test.network_security_group_id}"
+  public_ip_id        = "${module.publicip.public_ip_address_id}"
+}
---
+++ terraform/modules/vm/input.tf
@@ -0,0 +1,8 @@
+
+# Resource Group/Location
+variable "location" {}
+variable "resource_type" {}
+variable "resource_group" {}
+variable "subnet_id" {}
+variable "public_ip_id" {}
+variable "security_group_id" {}
